80bccf19608c8b5ecd058b4a44ad55107b22e9f8
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-07-23 15:12:21 -0300

    changes the App component to fetch data of a post

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useQuery } from "react-query";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isLoading, error, data } = useQuery("posts", () =>
+    fetch("https://jsonplaceholder.typicode.com/posts/1").then((res) =>
+      res.json()
+    )
+  );
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Básico do React Query</h1>
+      <ul>
+        <p key={data.id}>{data.title}</p>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
